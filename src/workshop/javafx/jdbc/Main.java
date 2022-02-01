abf27b1266603c3a673ae5bfa21ed071e2243ec0
Janathan <jonathan2018.matos@gmail.com>
2022-02-01 16:30:23 -0300

    Main View

--- src/workshop/javafx/jdbc/Main.java
+++ src/workshop/javafx/jdbc/Main.java
@@ -5,9 +5,12 @@
  */
 package workshop.javafx.jdbc;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
@@ -19,8 +22,8 @@ import javafx.stage.Stage;
  */
 public class Main extends Application {
     
-    @Override
-    public void start(Stage primaryStage) {
+    //@Override codigo que vem oriundo da iniciação do projeto
+    /*public void start(Stage primaryStage) {
         Button btn = new Button();
         btn.setText("Say 'Hello World'");
         btn.setOnAction(new EventHandler<ActionEvent>() {
@@ -39,6 +42,22 @@ public class Main extends Application {
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
         primaryStage.show();
+    }*/
+    @Override // metodo criado por mim
+    public void start (Stage primaryStage){
+        
+        try{
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+            Parent parent = loader.load();
+            Scene mainScene = new Scene(parent);
+            primaryStage.setScene(mainScene);
+            primaryStage.setTitle("Sample JavaFX application");
+            primaryStage.show();
+            
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        
     }
 
     /**
