d127b4cf650a8128aedccd03b06a0db8d01e3db0
Janathan <jonathan2018.matos@gmail.com>
2022-02-02 21:22:01 -0300

    DepartmentService

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,7 +1,10 @@
 package gui;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -10,6 +13,7 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import model.entities.Department;
+import model.services.DepartmentService;
 import workshop.javafx.jdbc.Main;
 
 /**
@@ -17,6 +21,8 @@ import workshop.javafx.jdbc.Main;
  * @author jonathan
  */
 public class DepartmentListController implements Initializable {
+       
+    private DepartmentService service;
 
     @FXML
     private TableView<Department> tableViewDepartment;
@@ -27,12 +33,18 @@ public class DepartmentListController implements Initializable {
     @FXML
     private TableColumn<Department, String> tableColumnName;
     
+    private ObservableList<Department> obsList;
+    
     @FXML
     private Button btNew;
     
     @FXML
     public void onBtNewAction(){
         System.out.println("On button new action");
+    }
+    
+    public void setDepartmentService(DepartmentService service){
+        this.service = service;
     }
     
     @Override
@@ -50,5 +62,15 @@ public class DepartmentListController implements Initializable {
          tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
          
     }
-
+    
+    public void updateTableView(){
+        
+        if(service == null){
+            throw new IllegalStateException("Service was null");
+        }
+        
+        List<Department> list = service.findAll();
+        obsList = FXCollections.observableArrayList(list);
+        tableViewDepartment.setItems(obsList);
+    }
 }
