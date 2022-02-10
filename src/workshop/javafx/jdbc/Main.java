82e75de1f3048db8c8dd0eb97f3f2df0cd3b1cd9
Janathan <jonathan2018.matos@gmail.com>
2022-02-10 15:17:41 -0300

    Passando o objeto Department para o DepartmentForm view

--- src/workshop/javafx/jdbc/Main.java
+++ src/workshop/javafx/jdbc/Main.java
@@ -7,14 +7,9 @@ package workshop.javafx.jdbc;
 
 import java.io.IOException;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
