1a4dd662a70a21777706e61727fa3c9839167e97
Janathan <jonathan2018.matos@gmail.com>
2022-02-12 20:16:45 -0300

    Remove department

--- src/gui/util/Alerts.java
+++ src/gui/util/Alerts.java
@@ -5,19 +5,31 @@
  */
 package gui.util;
 
+import java.util.Optional;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
 
 /**
  *
  * @author jonathan
  */
 public class Alerts {
-    
-    public static void showAlert(String title, String header, String content, Alert.AlertType type){
+
+    public static void showAlert(String title, String header, String content, Alert.AlertType type) {
         Alert alert = new Alert(type);
         alert.setTitle(title);
         alert.setHeaderText(header);
         alert.setContentText(content);
         alert.show();
     }
+
+    public static Optional<ButtonType> showConfirmation(String title, String content) {
+        Alert alert = new Alert(AlertType.CONFIRMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(content);
+        return alert.showAndWait();
+    }
+
 }
